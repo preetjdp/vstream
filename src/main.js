4802c53993304744c85f7acb2668a31bc9b932af
Preet Parekh <preetjdp123@gmail.com>
2019-01-21 01:47:47 +0530

    Initialised VueMaterial in main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default-dark.css";
+
+Vue.use(VueMaterial)
+
 Vue.config.productionTip = false
 
 new Vue({
